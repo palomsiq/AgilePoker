e0af78171779d8d55f3ba4fb9b0a8e281c76ed77
palomsiq <palomanunes375@gmail.com>
2023-11-28 21:19:05 +0000

    First Files

---
+++ src/main/java/com/agile/poker/agilepoker/AgilePokerApplication.java
@@ -0,0 +1,13 @@
+package com.agile.poker.agilepoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilePokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilePokerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/agile/poker/agilepoker/application/PlanningSessionController.java
@@ -0,0 +1,33 @@
+package com.agile.poker.agilepoker.application;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agile.poker.agilepoker.business.CreatePlanningSession;
+import com.agile.poker.agilepoker.business.PlanningSession;
+
+
+
+@RestController
+@RequestMapping("/planningSession")
+
+public class PlanningSessionController{
+    
+    
+    private CreatePlanningSession createPlanningSession;
+    public PlanningSessionController(CreatePlanningSession createPlanningSession){
+       this.createPlanningSession = createPlanningSession;
+    }
+
+    @PostMapping
+    public void newSession(@RequestBody InputPlanningSession inputPlanningSession){
+        
+        PlanningSession planningSession = PlanningSession.create(inputPlanningSession.title(), inputPlanningSession.deckType());
+        System.out.println(createPlanningSession.execute(planningSession));
+    }
+
+
+}
+
